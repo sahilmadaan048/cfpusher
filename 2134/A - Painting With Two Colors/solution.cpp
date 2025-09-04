44d54cccd05f4584f438e6c78e3ddab115166ba6
Sahil Madaan <150500600+sahilmadaan048@users.noreply.github.com>
2025-09-04 22:58:11 +0530

    Add Painting With Two Colors [A] from Codeforces

---
+++ 2134/A - Painting With Two Colors/solution.cpp
@@ -0,0 +1,1 @@
+#include <iostream>using namespace std; int main() {  ios::sync_with_stdio(false);  cin.tie(0);  int t;  cin >> t;  while (t--) {    int n, a, b;    cin >> n >> a >> b;    if (a <= b) {      cout << ((n % 2) == (b % 2) ? "YES\n" : "NO\n");    } else {      cout << ((n % 2) == (b % 2) && (n % 2) == (a % 2) ? "YES\n" : "NO\n");    }  }}
