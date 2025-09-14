1fada3997195ae987c4ca21bed7e52db9e23744a
Sahil Madaan <150500600+sahilmadaan048@users.noreply.github.com>
2025-09-14 22:08:34 +0530

    Add Minutes Before the New Year [A] from Codeforces

---
+++ 1283/A - Minutes Before the New Year/solution.cpp
@@ -0,0 +1,1 @@
+#include <bits/stdc++.h> using namespace std; int main() {#ifdef _DEBUG	freopen("input.txt", "r", stdin);//	freopen("output.txt", "w", stdout);#endif		int q;	scanf("%d", &q);	for (int i = 0; i < q; ++i) {		int h, m;		scanf("%d %d", &h, &m);		printf("%d\n", 1440 - h * 60 - m);	}		return 0;}
