856e3e6461cda316bc355b60ce1ddc1cd12644b1
Sahil Madaan <150500600+sahilmadaan048@users.noreply.github.com>
2025-12-26 18:49:21 +0530

    Add Twin Permutations [A] from Codeforces

---
+++ 1831/A - Twin Permutations/solution.cpp
@@ -0,0 +1,1 @@
+// https://codeforces.com/problemset/problem/1831/A #include <bits/stdc++.h>using namespace std; void solve() {    int n;    cin >> n;    vector<int> a(n);     for(int i=0; i<n; i++) cin >> a[i];     for(auto ele: a) {        cout << n + 1 - ele << " ";    }    cout << '\n';} int main() {    int t; cin >> t;    while(t--) {        solve();    }}
