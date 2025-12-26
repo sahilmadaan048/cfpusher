1675f1a71922499b19324433d2c62c50781e887c
Sahil Madaan <150500600+sahilmadaan048@users.noreply.github.com>
2025-12-26 14:48:17 +0530

    Add Jagged Swaps [A] from Codeforces

---
+++ 1896/A - Jagged Swaps/solution.cpp
@@ -0,0 +1,1 @@
+// https://codeforces.com/problemset/problem/1896/A #include <bits/stdc++.h>using namespace std; void solve() {    int n; cin >> n;    vector<int> a(n);     for(int i=0; i<n; i++) cin >> a[i];     if(a[0] == 1) {        cout << "YES\n";    } else cout << "NO\n";} int main() {    int t; cin >> t;     while(t--) {        solve();    }}
